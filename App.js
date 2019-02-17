99f7599ea76b9b03f07ee12838a6d85b704356df
strimaitis <33992296+strimaitis@users.noreply.github.com>
2019-02-17 13:33:04 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -8,7 +8,11 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Button, Alert, AsyncStorage} from 'react-native';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
+import DefaultScreen from './screens/DefaultScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,103 +22,39 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-var keys = [''];
-
-export default class App extends Component<Props> {
-
-constructor(props) {
-   	super(props)
-   	this.state = {
-   		username: '',
-   		password: '',
-   	}
-   }
-
+class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-      	<Text style={styles.welcome}>
-      	Register:</Text>
-      	
-    	<Text style={styles.instructions}>
-    	Username</Text>
-    	
-    	<TextInput style={styles.input} 
-    	onChangeText = {
-    		username => this.setState({username})
-    	}
-    	// placeholder = 'username'
-    	/>
-    	
-    	<Text style={styles.instructions}>
-    	Password</Text>
-    	
-    	<TextInput style={styles.input} 
-    	onChangeText = {
-    		password => this.setState({password})
-    	}
-    	// placeholder = 'password'
-    	secureTextEntry={true} />
-    	
-    	<Button 
-    	onPress={this.SubmitPressed.bind(this)}
-    	title="Submit"/>
-    	
-    	<Button 
-    	onPress={this.showUsers.bind(this)}
-    	title="Show"/>
-    	
-    	<Button 
-    	onPress={this.clearUsers.bind(this)}
-    	title="Clear"/>
-      </View>
+    	<AppStackNavigator />
     );
   }
-  
-  async SubmitPressed() {
-	  const {username, password} = this.state;
-	  keys[0] = username.trim();
-	  if (await AsyncStorage.getItem(keys[0])) {
-	  	alert("Username already exists!");
-	  } else {
-	  	AsyncStorage.setItem(keys[0], password);
-	  }
-	}
-	
-async showUsers() {
-	await AsyncStorage.getItem(keys[0]).then((value) => {
-	  	alert(value);
-	  }).done();
 }
 
-async clearUsers() {
-	AsyncStorage.clear();
-}
-	
-}
+const AppStackNavigator = createStackNavigator({
+	Default: DefaultScreen,
+	Register: RegisterScreen,
+	Profile: ProfileScreen
+});
+
+const AppContainer = createAppContainer(AppStackNavigator);
+
+export default AppContainer;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ffffff',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-    lineHeight: 55
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
-  },
-  input: {
-  	height: 30, 
-  	width: 160,
-  	borderColor: 'gray', 
-  	borderWidth: 1,
   },
 });
